68630047dc837b61c578c319e65180944b374714
flo071 <flo071@gmx.at>
2021-04-29 01:43:44 +0200

    fix stake tx view

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1871,7 +1871,7 @@ static UniValue getblockstats(const JSONRPCRequest& request)
             }
         }
 
-        if (tx->IsCoinBase()) {
+        if (tx->IsCoinBase() || tx->IsCoinStake()) {
             continue;
         }
 
